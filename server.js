9ee3aba8518fae266ee422737db7d9ab2d0305ae
syniavskyi <syniavskyi.pavlo@gmail.com>
2018-04-03 13:43:08 +0200

    Root dirname added

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
-app.use(serveStatic(__dirname + "/dist"));
+app.use(serveStatic(__dirname + "/"));
+// app.get("/", function(req, res) {
+//     res.send("index.html");
+// });
 var port = process.env.PORT || 5000;
 app.listen(port);
-console.log('server started ' + port);
+console.log('Listening on http://localhost:' + port + '/');
